35cf74c0a7c3e31de74fdb28666643e8e47ac047
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-09 18:44:27 +0000

    Stable Version

--- main.py
+++ main.py
@@ -14,25 +14,26 @@ Things we need to program:
 5. Quests
 """
 
-
 __author__ = "Mr. McGarrah's 7th Period"
 __version__ = "0.1"
 
 
 class Character:
-  def __init__(self, name, charisma=5, health=100):
-    self.name = name
-    self.charisma = charisma
-    self.health = health
+
+    def __init__(self, name, charisma=5, health=100):
+        self.name = name
+        self.charisma = charisma
+        self.health = health
 
 
 def main() -> None:
-  """ Main entry point for the game """
-  player = Character(name="Player", health=200)
-  print(player.name)
-  print(player.health)
+    """ Main entry point for the game """
+    player = Character(name="Player", health=200)
+    shopkeeper = Character(name="Keeper of Shops")
+    print(shopkeeper.name)
+    print(shopkeeper.health)
 
 
 if __name__ == "__main__":
-  """ This is excecuted when the file is run from the command line """
-  main()
+    """ This is excecuted when the file is run from the command line """
+    main()
