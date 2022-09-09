2cc3e57578881fce72023faa95713b723d27e193
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-09 19:01:21 +0000

    Added class `Item`.
    Has class `Character`, function `main`, objects `player`, `shopkeeper`, and `thief`.

--- main.py
+++ main.py
@@ -20,16 +20,22 @@ __version__ = "0.1"
 
 class Character:
 
-    def __init__(self, name, charisma=5, health=100):
+    def __init__(self, name, attack={"fist": 10}, charisma=5, health=100):
         self.name = name
         self.charisma = charisma
         self.health = health
+        self.attack = attack
 
+class Item:
+    def __init__(self, name, price):
+        
 
 def main() -> None:
     """ Main entry point for the game """
+    #  Create characters, NPCs, and enemies
     player = Character(name="Player", health=200)
     shopkeeper = Character(name="Keeper of Shops")
+    thief = Character(name="Zaam", attack={"dagger": 20})
     print(shopkeeper.name)
     print(shopkeeper.health)
 
