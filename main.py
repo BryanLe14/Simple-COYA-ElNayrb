c3ef2d6c96116ee844beb1ffed87d406ce40e41c
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-12 19:19:21 +0000

    Ceated class `Potion`. Created weapon and potion objects.

--- main.py
+++ main.py
@@ -20,11 +20,12 @@ __version__ = "0.1"
 
 class Character:
 
-    def __init__(self, name, attack={"fist": 10}, charisma=5, health=100):
+    # def __init__(self, name, attack={"fist": 10}, charisma=5, health=100):
+    def __init__(self, name, weapon=None, charisma=5, health=100):
         self.name = name
+        self.weapon = weapon
         self.charisma = charisma
         self.health = health
-        self.attack = attack
 
 class Item:
     def __init__(self, name, price):
@@ -34,21 +35,41 @@ class Item:
 
 class Weapon(Item):
     def __init__(self, name, price, attack, range=False):
-        Item.__init__(self, name, price)
+        super().__init__(name, price)
         self.attack = attack
         self.range = range
 
+class Potion(Item):
+    def __init__(self, name: str, price: int, effect: str, amount=0):
+        super().__init__(name, price)
+        self.effect = effect
+        self.amount = amount
+
 def main() -> None:
     """ Main entry point for the game """
-    #  Create characters, NPCs, and enemies
-    player = Character(name="Player", health=200)
-    shopkeeper = Character(name="Keeper of Shops")
-    thief = Character(name="Zaam", attack={"dagger": 20})
-
     # Create weapons
     sword = Weapon("Toby", 25, 50)
+    fist = Weapon("fist", 0, 10)
+    dagger = Weapon("Dolly Dagger", 15, 20)
+    weapon_list = [sword, fist, dagger]
+    
+    # Create potions
+    health_potion = Potion("Health Potion", 25, "heal", 50)
+    poison_potion = Potion("Poison Potion", 35, "poison", 10)
+    love_potion = Potion("Love Potion", 50, "love", 25)
+    potion_list = [health_potion, poison_potion, love_potion]
+    
+    #  Create characters, NPCs, and enemies
+    player = Character(name="Player", weapon=fist, health=200)
+    shopkeeper = Character(name="Keeper of Shops", weapon=sword)
+    thief = Character(name="Zaam", weapon=dagger, health=100)
+
+    # Testing functionality
     print(shopkeeper.name)
+    print(shopkeeper.weapon.name)
     print(shopkeeper.health)
+
+    
 
 
 if __name__ == "__main__":
