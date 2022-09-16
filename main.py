c7c2eff5718780f08108b8fc2da5eb02eb3d6282
ElNayrb <99197660+BryanLe14@users.noreply.github.com>
2022-09-16 19:32:08 +0000

    Stable version. Started implementing fog world.

--- main.py
+++ main.py
@@ -34,23 +34,53 @@ from os import system
 from functions import *
 from classes import *
 from getkey import getkey, keys
+import re
 
 # World map and player starting location
 world = [
         "######################",
+        "#    K           S   #",
         "#                    #",
+        "# TP        L        #",
         "#                    #",
+        "#    L               #",
         "#                    #",
-        "#                    #",
-        "#                    #",
-        "#                    #",
-        "#                    #",
-        "#                    #",
-        "#                    #",
-        "#                    #",
+        "#        C    L      #",
+        "# S                  #",
+        "#               B    #",
+        "#    C               #",
         "######################"
-    ]  
-location = [3, 3]
+    ]
+# Makes a fog world map
+fog_world = []
+for row in range(len(world)):
+    fog_world.append("")
+    for col in range(len(world[row])):
+        if world[row][col] != "#":
+            fog_world[row] += "/"
+        else:
+            fog_world[row] += "#"
+print(fog_world)
+
+temp_re = re.compile(r"[^\#]")
+fog_world = [re.sub(temp_re, "/", x) for x in world]
+
+print(fog_world)
+
+# print(id(world))
+# print(id(fog_world))
+input()
+
+key = "BCKLPST" # Boss, Cave, King, Love, Player, Store, Tavern
+items = {}
+for row in range(len(world)):
+    for item in world[row]:
+        if item not in items and item in key:
+            items[item] = [(row, world[row].index(item))]
+        elif item in items:
+            items[item].append((row, world[row].index(item)))
+
+location = list(items["P"][0])
 
 def main(world, location) -> None:    
     """ Main entry point for the game """
@@ -79,7 +109,7 @@ def main(world, location) -> None:
         world = update_ascii_map(world, row, col)
         print_map(world)
         world_list = map2dlist(world)
-        print(location)
+        # print(location)
         
         direction = getkey()
         world_list, location = check_collision(direction, world_list, location)
